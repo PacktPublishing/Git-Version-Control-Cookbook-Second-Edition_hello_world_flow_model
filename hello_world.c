f872a70d07baf48a2ebd2d8e2f94d17f85bf6ae9
John Doe <john.doe@example.com>
2014-03-19 15:28:03 +0100

    Say hello to all of the continents
    Says hello to all continents in alphabetical order

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void continents(void) {
+	printf("Hello, Africa\n");
+	printf("Hello, Antarctica\n");
+	printf("Hello, Asia\n");
+	printf("Hello, Australia\n");
+	printf("Hello, Europe\n");
+	printf("Hello, North America\n");
+	printf("Hello, South America\n");
+	printf("\n");
+}
+
+
 int main(void) {
+	/* Say hello to all the continents */
+	continents();
+
 	printf("Hello, world\n");
 
 	return 0;
